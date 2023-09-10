65a3ed575be821b873d62e87fbd3611182eba923
oo-ng <122175989+oo-ng@users.noreply.github.com>
2023-09-10 17:07:21 -0400

    finsihed menu

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -7,6 +7,7 @@ import { onAuthStateChanged, getAuth, signOut } from "@firebase/auth";
 export const PageNav = () => {
     const [signedIn, setSignedIn] = useState();
     const [userInfo, setUserInfo] = useState();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleSignOut = () => {
         const auth = getAuth(FBInstance);
@@ -31,35 +32,38 @@ export const PageNav = () => {
     return(
         
         <nav className={styles.nav}>
-           <ul className={styles.ul}>
+             <div className={styles.hamburger} onClick={() => setIsMenuOpen(!isMenuOpen)}>{isMenuOpen?"X":"☰"}</div>
             
-                <li>
-                    <NavLink to="/">
-                        Home
-                    </NavLink>
-                </li>
-                
-                <li>
-                    <NavLink to="/Services">
-                        Services
-                    </NavLink>
-                </li>
+                    { isMenuOpen?<ul className={`${styles.ul} ${isMenuOpen ? styles.menuOpen : ''}`}>
+                    
+                    <li>
+                        <NavLink to="/">
+                            Home
+                        </NavLink>
+                    </li>
+                    
+                    <li>
+                        <NavLink to="/Services">
+                            Services
+                        </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/contactus">
-                        Contact us
-                    </NavLink>
-                </li>
+                    <li>
+                        <NavLink to="/contactus">
+                            Contact us
+                        </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/trackpackage">
-                        Track your Package
-                    </NavLink>
-                </li>
+                    <li>
+                        <NavLink to="/trackpackage">
+                            Track your Package
+                        </NavLink>
+                    </li>
 
-                {signedIn?<span>Welcome back {userInfo.displayName? userInfo.displayName.split(' ')[0]:userInfo.email}</span>:""}
-                
-            </ul> 
+                    {signedIn?<span>Welcome back {userInfo.displayName? userInfo.displayName.split(' ')[0]:userInfo.email}</span>:""}
+                    
+                </ul>: ""} 
+            
             <>
             {signedIn?<li>
                 <NavLink to="/"
